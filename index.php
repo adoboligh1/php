b34c8f7a420c02c382a2791ec917370e76f7eb5d
Albert Adoboli <43940776+adoboligh1@users.noreply.github.com>
2021-09-03 22:25:00 +0000

    Update index.php

--- index.php
+++ index.php
@@ -152,7 +152,7 @@ if(!empty($url)) {
 
 			$ice_stats = json_decode($data, true);
 
-			if(is_array($ice_stats["icestats"]["source"])) {
+			if (is_array($ice_stats["icestats"]["source"]) && isset($ice_stats["icestats"]["source"][0])) {
 				$ice_stats_source = $ice_stats["icestats"]["source"][0];
 			} else {
 				$ice_stats_source = $ice_stats["icestats"]["source"];
@@ -208,3 +208,30 @@ header('Access-Control-Allow-Origin: '.$urlHost);
 header('Content-type: application/json', true);
 
 echo json_encode($array);
+
+function cors() {
+    
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    
+        exit(0);
+    }
+    
+    echo "You have CORS!";
+}
